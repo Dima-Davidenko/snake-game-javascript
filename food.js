5fd15f00ae8451c4816670df7fb9cf953674e083
Dmytro Davidenko <dima080885@gmail.com>
2023-01-02 14:07:06 +0200

    food draw function

--- food.js
+++ food.js
@@ -10,3 +10,11 @@ export function update() {
     food = getRandomFoodPosition()
   }
 }
+
+export function draw(gameBoard) {
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+}
